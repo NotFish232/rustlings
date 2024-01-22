d31936d751e0b8ccd62d018d1a454a4893c6bfdb
Justin Lee <2025jlee@tjhsst.edu>
2024-01-22 12:53:18 -0500

    initial commit

---
+++ 22_clippy/target/debug/deps/clippy3-25e416aaaf243608.d
@@ -0,0 +1,9 @@
+/home/justin/code/rust/rustlings/exercises/22_clippy/target/debug/deps/libclippy3-25e416aaaf243608.rmeta: clippy3.rs Cargo.toml
+
+/home/justin/code/rust/rustlings/exercises/22_clippy/target/debug/deps/clippy3-25e416aaaf243608.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
